fd2b07b1f2c2c7ed801b648d33dd1f42a524e417
alexander parco flores <alexander.parco@somosari.com>
2024-04-24 21:09:38 -0500

    test

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
 
 if(process.env.NODE_ENV === "development") {
     require('dotenv').config()
 }
 
  
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+app.listen(port, () => {
+    console.log('running...' )
+})
